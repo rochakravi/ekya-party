9e2af9dd7f862d30c3d105c5f61010c18bdb749e
Ravi Raj Singh <rochak.ravi@gmail.com>
2018-09-19 05:29:05 -0400

    pointing to dev url now

--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -14,15 +14,15 @@ export class ApiService {
   }
 
   getData(): Observable<any>{
-      return this.http.get('https://ekta-p.herokuapp.com/suggestions') //prod
-    //  return this.http.get('https://ekta-pa-dev.herokuapp.com/suggestions') //dev
+     // return this.http.get('https://ekta-p.herokuapp.com/suggestions') //prod
+      return this.http.get('https://ekta-pa-dev.herokuapp.com/suggestions') //dev
     //return this.http.get('http://localhost:8080/suggestions') //dev
    
   }
   sendData(data){
   	console.log(data);
-    let url = 'https://ekta-p.herokuapp.com/suggestion' ; // prod
-    //let url = 'https://ekta-pa-dev.herokuapp.com/suggestion' ; // dev
+    //let url = 'https://ekta-p.herokuapp.com/suggestion' ; // prod
+    let url = 'https://ekta-pa-dev.herokuapp.com/suggestion' ; // dev
     //let url = 'http://localhost:8080/suggestion' ; // local
   	return this.http.post(url, data)
   }
